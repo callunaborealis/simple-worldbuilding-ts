f53dd2c5c836b4262287a6597a67db58466e98fa
callunaborealis <callunaborealis@gmail.com>
2021-07-25 15:12:01 +0800

    Fix missing template.json
    Though it still doesn't work

--- dist.sh
+++ dist.sh
@@ -1,3 +1,7 @@
 rm -rf dist &&
   mkdir dist &&
-  cp system.json ./dist/system.json && zip -vr dist/system.zip build -x '*.git*' -x ".DS_Store"
+  # Ensure system.json is inside dist for release
+  cp src/system.json dist/system.json &&
+  # Ensure template.json is inside the system.zip source
+  cp src/template.json build/template.json &&
+  zip -vr dist/system.zip build -x '*.git*' -x ".DS_Store" -x '*.ts*'
--- release.sh
+++ release.sh
@@ -1,1 +1,2 @@
-gh release create v0.1.0 dist/system.json dist/system.zip --notes "Link to manifest URL: https://github.com/callunaborealis/simple-worldbuilding-ts/releases/latest/download/system.json"
+gh release create v0.1.0 dist/system.json dist/system.zip \
+  --notes "Link to manifest URL: https://github.com/callunaborealis/simple-worldbuilding-ts/releases/latest/download/system.json"
